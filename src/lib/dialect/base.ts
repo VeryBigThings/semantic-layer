0f1c081d90727c097736e95e854e0615381f70c7
Mihael Konjevic <konjevic@gmail.com>
2024-04-14 19:55:06 +0200

    fix: date/time granularities use 0 prefix where appropriate

--- src/lib/dialect/base.ts
+++ src/lib/dialect/base.ts
@@ -14,21 +14,21 @@ export class BaseDialect {
       case "quarter_of_year":
         return `EXTRACT(QUARTER FROM ${sql})`;
       case "month":
-        return `EXTRACT (YEAR FROM ${sql}) || '-' || EXTRACT(MONTH FROM ${sql})`;
+        return `EXTRACT (YEAR FROM ${sql}) || '-' || LPAD(EXTRACT(MONTH FROM ${sql})::varchar, 2, '0')`;
       case "month_num":
         return `EXTRACT(MONTH FROM ${sql})`;
       case "week":
-        return `EXTRACT (YEAR FROM ${sql}) || '-' || 'W' || EXTRACT(WEEK FROM ${sql})`;
+        return `EXTRACT (YEAR FROM ${sql}) || '-' || 'W' || LPAD(EXTRACT(WEEK FROM ${sql})::varchar, 2, '0')`;
       case "week_num":
         return `EXTRACT(WEEK FROM ${sql})`;
       case "day_of_month":
         return `EXTRACT(DAY FROM ${sql})`;
       case "hour":
-        return `CAST(${sql} AS DATE) || ' ' || EXTRACT(HOUR FROM ${sql})`;
+        return `CAST(${sql} AS DATE) || ' ' || LPAD(EXTRACT(HOUR FROM ${sql})::varchar, 2, '0')`;
       case "hour_of_day":
         return `EXTRACT(HOUR FROM ${sql})`;
       case "minute":
-        return `CAST(${sql} AS DATE) || ' ' || EXTRACT(HOUR FROM ${sql}) || ':' || EXTRACT(MINUTE FROM ${sql})`;
+        return `CAST(${sql} AS DATE) || ' ' || LPAD(EXTRACT(HOUR FROM ${sql})::varchar, 2, '0') || ':' || LPAD(EXTRACT(MINUTE FROM ${sql})::varchar, 2, '0')`;
 
       default:
         // biome-ignore lint/correctness/noSwitchDeclarations: Exhaustiveness check
