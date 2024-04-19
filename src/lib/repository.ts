307a43d91306db8d48b24bdd7b0181832c5caffa
Mihael Konjevic <konjevic@gmail.com>
2024-04-19 22:13:15 +0200

    refactor: refactor repository, query bbuilder and build query files
    Move things to a more appropriate place, pass queryBuilder around instead of repository, client and
    dialect. All of these live on query builder class

--- src/lib/repository.ts
+++ src/lib/repository.ts
@@ -16,11 +16,17 @@ import {
   GetFilterFragmentBuilderRegistryPayload,
   defaultFilterFragmentBuilderRegistry,
 } from "./query-builder/filter-builder.js";
-import { AvailableDialects, FilterType, MemberNameToType } from "./types.js";
+import {
+  AnyQueryFilter,
+  AvailableDialects,
+  FilterType,
+  MemberNameToType,
+} from "./types.js";
 
 import graphlib from "@dagrejs/graphlib";
 import knex from "knex";
 import invariant from "tiny-invariant";
+import { z } from "zod";
 import { BaseDialect } from "./dialect/base.js";
 import { QueryBuilder } from "./query-builder.js";
 
@@ -60,6 +66,85 @@ function getClientAndDialect(dialect: AvailableDialects): {
       throw new Error(`Dialect ${dialect} not supported`);
   }
 }
+
+function getDimensionNamesSchema(dimensionPaths: string[]) {
+  return z
+    .array(
+      z
+        .string()
+        .refine((arg) => dimensionPaths.includes(arg))
+        .describe("Dimension name"),
+    )
+    .optional();
+}
+
+function getMetricNamesSchema(metricPaths: string[], dimensionPaths: string[]) {
+  const adHocMetricSchema = z.object({
+    aggregateWith: z.enum(["sum", "count", "min", "max", "avg"]),
+    dimension: z
+      .string()
+      .refine((arg) => dimensionPaths.includes(arg))
+      .describe("Dimension name"),
+  });
+
+  return z
+    .array(
+      z
+        .string()
+        .refine((arg) => metricPaths.includes(arg))
+        .describe("Metric name")
+        .or(adHocMetricSchema),
+    )
+    .optional();
+}
+
+export function buildQuerySchema(repository: AnyRepository) {
+  const dimensionPaths = repository.getDimensions().map((d) => d.getPath());
+  const metricPaths = repository.getMetrics().map((m) => m.getPath());
+  const memberPaths = [...dimensionPaths, ...metricPaths];
+
+  const registeredFilterFragmentBuildersSchemas = repository
+    .getFilterFragmentBuilderRegistry()
+    .getFilterFragmentBuilders()
+    .map((builder) => builder.fragmentBuilderSchema);
+
+  const filters: z.ZodType<AnyQueryFilter[]> = z.array(
+    z.union([
+      z.object({
+        operator: z.literal("and"),
+        filters: z.lazy(() => filters),
+      }),
+      z.object({
+        operator: z.literal("or"),
+        filters: z.lazy(() => filters),
+      }),
+      ...registeredFilterFragmentBuildersSchemas.map((schema) =>
+        schema.refine((arg) => memberPaths.includes(arg.member), {
+          path: ["member"],
+          message: "Member not found",
+        }),
+      ),
+    ]),
+  );
+
+  const schema = z
+    .object({
+      dimensions: getDimensionNamesSchema(dimensionPaths),
+      metrics: getMetricNamesSchema(metricPaths, dimensionPaths),
+      filters: filters.optional(),
+      limit: z.number().optional(),
+      offset: z.number().optional(),
+      order: z.record(z.string(), z.enum(["asc", "desc"])).optional(),
+    })
+    .refine(
+      (arg) => (arg.dimensions?.length ?? 0) + (arg.metrics?.length ?? 0) > 0,
+      "At least one dimension or metric must be selected",
+    );
+
+  return schema;
+}
+
+export type QuerySchema = ReturnType<typeof buildQuerySchema>;
 
 export class Repository<
   C,
@@ -124,14 +209,13 @@ export class Repository<
   }
 
   getFilterBuilder(
-    repository: AnyRepository,
     dialect: BaseDialect,
     filterType: FilterType,
     referencedModels: string[],
     metricPrefixes?: Record<string, string>,
   ): FilterBuilder {
     return this.filterFragmentBuilderRegistry.getFilterBuilder(
-      repository,
+      this,
       dialect,
       filterType,
       referencedModels,
@@ -287,7 +371,13 @@ export class Repository<
 
   build(dialectName: AvailableDialects) {
     const { client, Dialect } = getClientAndDialect(dialectName);
-    return new QueryBuilder<C, D, M, F>(this, new Dialect(), client);
+    const querySchema = buildQuerySchema(this);
+    return new QueryBuilder<C, D, M, F>(
+      this,
+      querySchema,
+      new Dialect(),
+      client,
+    );
   }
 }
 
